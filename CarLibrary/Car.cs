651ae72f49075df9040b461bea6ecab933415f90
sigmade <sychyov1991@mail.ru>
2020-05-16 08:11:15 +0300

    Add project files.

---
+++ CarLibrary/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CarLibrary
+{
+    //engine state
+    public enum EngineState
+    { engineAlive, engineDead}
+
+    //Abstract base class
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        protected EngineState egnState = EngineState.engineAlive;
+        public EngineState EngineState => egnState;
+        public abstract void TurboBoost();
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            PetName = name;
+            MaxSpeed = maxSp;
+            CurrentSpeed = currSp;
+        }
+    }
+}
---
+++ CarLibrary/DerivedCars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+    public class SportsCar : Car
+    {
+        public SportsCar() { }
+        public SportsCar(string name, int maxSp, int currSp) : base (name, maxSp, currSp) { }
+        public override void TurboBoost()
+        {
+            MessageBox.Show("Very fast!");
+        }
+    }
+
+    public class MiniVan : Car
+    {
+        public MiniVan() { }
+        public MiniVan(string name, int maxSp, int currSp) : base (name, maxSp, currSp) { }
+        public override void TurboBoost()
+        {
+            MessageBox.Show("Engine exploded...");
+        }
+    }
+}
